614027f026a9232238db777dac6e4613e3d9d33f
fulvmei <fulvmei@qq.com>
2023-07-13 14:22:55 +0800

    Initial commit

---
+++ app/src/main/java/com/fulvmei/android/mavenpublish/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fulvmei.android.mavenpublish.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
